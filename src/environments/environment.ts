186830148cb73f3931644cb41dd85eaa7c88dfbd
Your Name <you@example.com>
2019-06-10 22:56:34 +0530

    secure url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  apiUrl: "http://shop365app.herokuapp.com/api/v1",
-  baseUrl: "http://shop365app.herokuapp.com"
+  apiUrl: "https://shop365app.herokuapp.com/api/v1",
+  baseUrl: "https://shop365app.herokuapp.com"
 };
 
 /*
